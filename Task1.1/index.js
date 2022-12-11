25db6fc8ace52fc804f8e735aff77703f593058f
Aliaksandr Mamonenka <mamonenkoaa@gmail.com>
2022-12-11 20:10:36 +0100

    Task2 convert cvs to JSON

--- Task1.1/index.js
+++ Task1.1/index.js
@@ -1,4 +1,4 @@
-process.stdin.on("data", data => {
-    data = data.toString().trim().split("").reverse().join("");
-    process.stdout.write(data + "\n")
-})
+process.stdin.on("data", (data) => {
+  data = data.toString().split("").reverse().join("");
+  process.stdout.write(data + "\n");
+});
