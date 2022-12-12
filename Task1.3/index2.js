0363c571d1eab2fed004474b8dabfd7343a4c7d3
Aliaksandr Mamonenka <mamonenkoaa@gmail.com>
2022-12-12 10:59:31 +0100

    Task 3 use pipe

--- Task1.3/index2.js
+++ Task1.3/index2.js
@@ -2,19 +2,13 @@ import fs from "fs";
 import path from "path";
 import csv from "csvtojson";
 
-const writableStream = fs.createWriteStream(
-  path.resolve(__dirname, "result.txt"),
-  { encoding: "utf8" }
-);
+const readStream = fs.createReadStream("./Task1.2/csv/nodejs-hw1-ex1.csv");
+const writeStream = fs.createWriteStream(path.resolve(__dirname, "result.txt"));
 
-const csvStream = csv({
-  ignoreColumns: /Amount/,
-}).fromFile("./Task1.2/csv/nodejs-hw1-ex1.csv");
-
-csvStream.on("data", (chunk) => {
-  writableStream.write(chunk.toString("utf8"));
-});
-
-csvStream.on("error", (err) => {
-  console.log(err);
-});
+readStream
+  .pipe(
+    csv({
+      ignoreColumns: /Amount/,
+    })
+  )
+  .pipe(writeStream);
